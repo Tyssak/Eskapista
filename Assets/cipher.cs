b5ea72ce906cace4b0456aa97e6f02933c858180
karoldb <karoldaniel756@gmail.com>
2024-01-31 12:21:00 +0100

    poprawki zagadki z szyfrem

--- Assets/cipher.cs
+++ Assets/cipher.cs
@@ -9,19 +9,21 @@ using TMPro;
 public class CipherController : MonoBehaviour
 {
     public TMP_Text Ciphered_Text;
-    public int shiftAmount = 1;  // Iloœæ miejsc do przesuniêcia
+    public TMP_Text Key_Deciphered;
 
     void Start()
     {
         Ciphered_Text = GameObject.FindWithTag("cipher_text").GetComponent<TextMeshPro>();
+        Key_Deciphered = GameObject.FindWithTag("decipher_key").GetComponent<TextMeshPro>();
     }
 
     private void Update()
     {
-
         // SprawdŸ, czy nast¹pi³o klikniêcie na kontrolerze VR
         if (Mouse.current.leftButton.wasPressedThisFrame) 
         {
+            int shiftAmount = 1;  // Iloœæ miejsc do przesuniêcia
+
             // Pobierz aktualny tekst z obiektu TextMesh
             string currentText = Ciphered_Text.text;
 
@@ -29,18 +31,45 @@ public class CipherController : MonoBehaviour
             string encryptedText = Encrypt(currentText, shiftAmount);
             Ciphered_Text.text = encryptedText;
 
-            Debug.Log("Encrypted Text: " + encryptedText);
+            //Debug.Log("Encrypted Text: " + encryptedText);
+        }
+
+        if (Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            int shiftAmount = -1;  // Iloœæ miejsc do przesuniêcia
+
+            // Pobierz aktualny tekst z obiektu TextMesh
+            string currentText = Ciphered_Text.text;
+
+            // Zaszyfruj tekst i zaktualizuj obiekt TextMesh
+            string encryptedText = Encrypt(currentText, shiftAmount);
+            Ciphered_Text.text = encryptedText;
+
+            //Debug.Log("Encrypted Text: " + encryptedText);
+        }
+
+        if (Ciphered_Text.text == "O cztery znaki w prawo")
+        {
+            Key_Deciphered.text = "Rozwi¹zano zagadkê - liczba: 4";
+            Key_Deciphered.color = new UnityEngine.Color(1, 0, 0, 1);
+        }
+        else
+        {
+            Key_Deciphered.text = "Rozwi¹¿ zagadkê";
+            Key_Deciphered.color = new UnityEngine.Color(0, 0, 0, 1);
         }
     }
 
     private string Encrypt(string text, int shift)
     {
         string result = "";
+
         foreach (char letter in text)
         {
             if (char.IsLetter(letter))
             {
-                char encryptedChar = (char)(((letter - 'A' + shift) % 26) + 'A');
+                char baseChar = char.IsUpper(letter) ? 'A' : 'a';
+                char encryptedChar = (char)(((letter - baseChar + shift) % 26 + 26) % 26 + baseChar);
                 result += encryptedChar;
             }
             else
@@ -48,7 +77,9 @@ public class CipherController : MonoBehaviour
                 result += letter;
             }
         }
+
         return result;
     }
+
 }
 
