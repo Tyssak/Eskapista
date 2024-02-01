c1a8797c103efdaecf7ac06fac634fb020368f93
karoldb <karoldaniel756@gmail.com>
2024-02-01 19:53:35 +0100

    Update cipher.cs

--- Assets/cipher.cs
+++ Assets/cipher.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem;
 using UnityEngine.XR;
 using UnityEngine.InputSystem.XR;
 using TMPro;
@@ -10,9 +10,26 @@ public class CipherController : MonoBehaviour
 {
     public TMP_Text Ciphered_Text;
     public TMP_Text Key_Deciphered;
+    public InputDevice leftCon;
+    public InputDevice rightCon;
+    //public bool _solved;
 
     void Start()
     {
+        
+        List<InputDevice> devicesRight = new List<InputDevice>();
+        List<InputDevice> devicesLeft = new List<InputDevice>();
+        Debug.Log(devicesRight.ToString());
+        InputDeviceCharacteristics rightControllerChar = InputDeviceCharacteristics.Right; // | InputDeviceCharacteristics.Controller;
+        InputDeviceCharacteristics leftControllerChar = InputDeviceCharacteristics.Left; // | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerChar, devicesRight);
+        InputDevices.GetDevicesWithCharacteristics(leftControllerChar, devicesLeft);
+        Debug.Log(devicesRight.ToString());
+        Debug.Log(devicesLeft.ToString());
+        rightCon = devicesRight[0];
+        leftCon = devicesLeft[0];
+        
+
         Ciphered_Text = GameObject.FindWithTag("cipher_text").GetComponent<TextMeshPro>();
         Key_Deciphered = GameObject.FindWithTag("decipher_key").GetComponent<TextMeshPro>();
     }
@@ -20,7 +37,8 @@ public class CipherController : MonoBehaviour
     private void Update()
     {
         // SprawdŸ, czy nast¹pi³o klikniêcie na kontrolerze VR
-        if (Mouse.current.leftButton.wasPressedThisFrame) 
+        //if (Mouse.current.leftButton.wasPressedThisFrame) 
+        if (rightCon.TryGetFeatureValue(CommonUsages.primaryButton, out bool rightConValue) && rightConValue)
         {
             int shiftAmount = 1;  // Iloœæ miejsc do przesuniêcia
 
@@ -34,7 +52,9 @@ public class CipherController : MonoBehaviour
             //Debug.Log("Encrypted Text: " + encryptedText);
         }
 
-        if (Mouse.current.rightButton.wasPressedThisFrame)
+
+        //if (Mouse.current.rightButton.wasPressedThisFrame)
+        if (leftCon.TryGetFeatureValue(CommonUsages.primaryButton, out bool leftConValue) && leftConValue)
         {
             int shiftAmount = -1;  // Iloœæ miejsc do przesuniêcia
 
@@ -48,15 +68,19 @@ public class CipherController : MonoBehaviour
             //Debug.Log("Encrypted Text: " + encryptedText);
         }
 
+        Ciphered_Text = GameObject.FindWithTag("cipher_text").GetComponent<TextMeshPro>();
+        Key_Deciphered = GameObject.FindWithTag("decipher_key").GetComponent<TextMeshPro>();
         if (Ciphered_Text.text == "O cztery znaki w prawo")
         {
-            Key_Deciphered.text = "Rozwi¹zano zagadkê - liczba: 4";
+            Key_Deciphered.text = "Rozwi¹zano zagadkê - liczba: 432";
             Key_Deciphered.color = new UnityEngine.Color(1, 0, 0, 1);
+            //_solved = true;
         }
         else
         {
             Key_Deciphered.text = "Rozwi¹¿ zagadkê";
             Key_Deciphered.color = new UnityEngine.Color(0, 0, 0, 1);
+            //_solved = false;
         }
     }
 
